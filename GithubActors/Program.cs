db63419e814318bff4f12e2587b15b3c9b26fb58
Cheranga Hatangala <cheranga@gmail.com>
2018-06-25 05:10:27 +1000

    Adding actors and forms from the completed solution from Petabridge.

--- GithubActors/Program.cs
+++ GithubActors/Program.cs
@@ -1,22 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Net;
 using System.Windows.Forms;
+using Akka.Actor;
+using Akka.Configuration.Hocon;
 
 namespace GithubActors
 {
     static class Program
     {
         /// <summary>
+        /// ActorSystem we'llbe using to collect and process data
+        /// from Github using their official .NET SDK, Octokit
+        /// </summary>
+        public static ActorSystem GithubActors;
+
+        /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            GithubActors = ActorSystem.Create("GithubActors");
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new GithubAuth());
         }
     }
 }
